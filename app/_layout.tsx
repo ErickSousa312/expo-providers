9c53605297682088de6614e0ff7361768072c550
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-12-05 17:43:23 -0300

    create commit]

---
+++ app/_layout.tsx
@@ -0,0 +1,71 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack, usePathname } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/components/useColorScheme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import CustomDrawerContent from '@/components/customDrawer';
+import { Text, View } from 'react-native';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+  const path = usePathname().replace(/^\//, ""); 
+  console.log(path)
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer 
+      screenOptions={{
+        headerTitle(props) {
+          return <View><Text style={{fontSize:22, paddingLeft:10, fontWeight:700}}>{path ==''?'Dólar Americano':path}</Text></View>;
+        },}}
+      initialRouteName={"Euro"}
+      drawerContent={CustomDrawerContent}>
+      </Drawer>
+      </GestureHandlerRootView>
+    </ThemeProvider>
+  );
+}
